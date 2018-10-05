5f2b30fa990d71d53c9c61c966c22b2cffda4d9c
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-05 23:34:57 +0300

    finishing the complaints menu

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Menu, Grid, Segment, Container, Card, Table } from 'semantic-ui-react';
+import { Button, Menu, TextArea, Table, Label } from 'semantic-ui-react';
 import axios from 'axios';
+import { env } from './env';
 
 class App extends Component {
   state = { 
     navActiveItem: 'home',
     menuActiveItem: 'complaints',
+    complaints: [],
   }
 
   handleNaveItemClick = (e, { name }) => this.setState({ navActiveItem: name })
   handleMenuItemClick = (e, { name }) => this.setState({ menuActiveItem: name })
 
-  async componentDidMount() {
-     axios.post('https://student-housing-backend-wadahesam.c9users.io/api/getAllRooms')
-     .then(response => {
-       console.log(response.data);
-     });
+  async componentWillMount() {
+      const complaints = await axios.post(`${env.url}/getAllComplaints`);
+      this.setState({
+        complaints: complaints.data,
+      });
+      console.log(this.state);
   }
 
   renderComplaintsMenu = () => {
+    const complaints = this.state.complaints.map((complaint, index) => {
+      return (
+        <Table.Row key={index}>
+          <Table.Cell>{index+1}</Table.Cell>
+          <Table.Cell>{complaint.title}</Table.Cell>
+          <Table.Cell>{complaint.description}</Table.Cell>
+          <Table.Cell>
+            {
+              (complaint.replay) ? 
+              complaint.replay : 
+              <div>
+                <TextArea autoHeight style={styles.tableTextArea}/>
+                <br/>
+                <Button size="small" fluid>Replay</Button>
+              </div>
+            }
+          </Table.Cell>
+          <Table.Cell>
+            {
+              (complaint.status === "open") ?
+              <Button inverted size="large" color="red">Close</Button> :
+              <Button inverted size="large" disabled color="red">Closed</Button>
+            }
+          </Table.Cell>
+        </Table.Row>
+      );
+    })
     return (
-      <Table style={styles.table}>
+      <Table celled style={styles.table} textAlign="center">
         <Table.Header>
           <Table.Row>
-            <Table.Cell>Hello World</Table.Cell>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Replay</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
+        <Table.Body>
+          {complaints}
+        </Table.Body>
       </Table>
     );
   }
@@ -109,6 +146,10 @@ const styles = {
   menuBody: {
     padding: 20,
     marginTop: -20,
+  },
+  tableTextArea: {
+    borderRadius: 5, 
+    width: '100%',
   }
 }
 
