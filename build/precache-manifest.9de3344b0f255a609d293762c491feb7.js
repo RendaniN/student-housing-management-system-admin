51fbca6584765f8fe6044b359d5f917f7b302da4
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-18 23:12:49 +0300

    build

---
+++ build/precache-manifest.9de3344b0f255a609d293762c491feb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd88bbcd5975aac57d19",
+    "url": "/student-housing-management-system-admin/static/js/runtime~main.bd88bbcd.js"
+  },
+  {
+    "revision": "417a16d15a131d6c3086",
+    "url": "/student-housing-management-system-admin/static/js/main.417a16d1.chunk.js"
+  },
+  {
+    "revision": "db79437f5137bc6c3ac3",
+    "url": "/student-housing-management-system-admin/static/js/1.db79437f.chunk.js"
+  },
+  {
+    "revision": "417a16d15a131d6c3086",
+    "url": "/student-housing-management-system-admin/static/css/main.b67bcb6b.chunk.css"
+  },
+  {
+    "revision": "db79437f5137bc6c3ac3",
+    "url": "/student-housing-management-system-admin/static/css/1.0bd2da2b.chunk.css"
+  },
+  {
+    "revision": "34ec296cb06118ecdfe508dd43ed2c70",
+    "url": "/student-housing-management-system-admin/index.html"
+  }
+];
